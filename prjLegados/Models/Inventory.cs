f5cb8c6d4e1317d3758865ceeebb1d85f3e3a26d
StevenCuasqui <cuasquisteven@gmail.com>
2018-07-09 16:34:25 -0500

    Inventario implementado

---
+++ prjLegados/Models/Inventory.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace prjLegados.Models
+{
+    public class Inventory
+    {
+        public int idInventario { get; set; }
+        public string nombreObjeto { get; set; }
+        public double precioUnit { get; set; }
+        public int cantidadDisponible { get; set; }
+        public DateTime fechaIngreso { get; set; }
+    }
+}
